f001cd1f445665a86b98469fe0c5390d12a49b09
Jr-Studios001 <jrorlando109197@gmail.com>
2025-10-29 21:27:56 -0500

    ¨modificacion de estilos de la pagina web

--- tienda-sena/resources/views/web/partials/header.blade.php
+++ tienda-sena/resources/views/web/partials/header.blade.php
@@ -1,8 +1,8 @@
-<header class="bg-dark py-5">
+<header class="bg-ls py-5">
     <div class="container px-4 px-lg-5 my-5">
         <div class="text-center text-white">
-            <h1 class="display-4 fw-bolder">Tienda Online</h1>
-            <p class="lead fw-normal text-white-50 mb-0">Tienda de productos tecnológicos</p>
+            <h1 class="display-4 fw-bolder">LogicSoftware</h1>
+            <p class="lead fw-normal text-white-50 mb-0">Transformamos logica en software...</p>
         </div>
     </div>
 </header>
--- tienda-sena/resources/views/web/partials/nav.blade.php
+++ tienda-sena/resources/views/web/partials/nav.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container px-4 px-lg-5">
-        <a class="navbar-brand" href="#!">ArtCode.com</a>
+        <a class="navbar-brand" href="#!">Logics.com</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span
                 class="navbar-toggler-icon"></span></button>
@@ -8,7 +8,7 @@
             <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
                 <li class="nav-item"><a class="nav-link active" aria-current="page" href="/">Inicio</a></li>
                 <li class="nav-item"><a class="nav-link" href="#">Acerca</a></li>
-                <li class="nav-item"><a class="nav-link" href="#">Tienda</a></li>
+                
                 
                 <li class="nav-item dropdown">
                     @auth
